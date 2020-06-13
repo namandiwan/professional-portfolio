561ced2c3a76aa9e9ec7278a789ec288153b1936
Diwan,Naman <Naman.Diwan@cerner.com>
2020-06-14 02:14:59 +0530

    Updates

---
+++ precache-manifest.f9814c7944f97cf5c3c3fa461a01b275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472e5d9047b6e8faf8689ab6fb2e1095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bcc9f414829943f4e9b",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e899a0b0b3df04210bd1",
+    "url": "/static/js/2.563cc229.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.563cc229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bcc9f414829943f4e9b",
+    "url": "/static/js/main.4ad16de8.chunk.js"
+  },
+  {
+    "revision": "1cf256291b36d4bb48f7",
+    "url": "/static/js/runtime-main.c2965e05.js"
+  }
+]);
